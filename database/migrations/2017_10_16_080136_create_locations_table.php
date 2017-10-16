c8b4635ea96bd784352b3350c2bfca4fe29b0c8c
younus93 <itsme@theyounus.com>
2017-10-16 13:32:39 +0530

    created locations

---
+++ database/migrations/2017_10_16_080136_create_locations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->increments('id')->index();
+            $table->string('state');
+            $table->string('district');
+            $table->string('locality');
+            $table->string('formatted_address');
+            $table->string('lat');
+            $table->string('lng');
+            $table->string('postal_code')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('locations');
+    }
+}
