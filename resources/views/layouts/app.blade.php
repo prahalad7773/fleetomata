7d95a68ae6012c15f22923286bf5046252f3509b
younus93 <itsme@theyounus.com>
2017-11-01 12:39:15 +0530

    datatable colvis button

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -96,8 +96,9 @@
 
 <!-- BEGIN PAGE LEVEL PLUGINS -->
 <script type="text/javascript" src={{ asset('js/lara-all.js') }}></script>
-<script type="text/javascript" src="https://cdn.datatables.net/v/dt/jszip-2.5.0/dt-1.10.16/b-1.4.2/b-html5-1.4.2/datatables.min.js"></script>
-
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/pdfmake.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/v/dt/jszip-2.5.0/dt-1.10.16/b-1.4.2/b-colvis-1.4.2/b-flash-1.4.2/b-html5-1.4.2/datatables.min.js"></script>
 <div class="ks-mobile-overlay"></div>
 <script>
     {{--@if(Session::has('notification'))--}}
@@ -115,7 +116,7 @@
     $('.dataTable').DataTable({
         aaSorting : [],
         buttons: [
-        'copy', 'excel', 'pdf'
+        'copy', 'excel', 'pdf','colvis'
         ],
         autoWidth : false,
         dom : "Bfrtip",
