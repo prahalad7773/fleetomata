ac50cce582ad81183a9b86aa977fa126c613f84b
younus93 <itsme@theyounus.com>
2017-12-01 22:01:50 +0530

    wip

---
+++ resources/views/trips/ledgers/partials/_create.blade.php
@@ -0,0 +1,72 @@
+<form action="{{ url("trips/{$trip->id}/ledgers") }}" method="post"> {!! csrf_field() !!}
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>When</th>
+                <th>From</th>
+                <th>To</th>
+                <th>Amount</th>
+                <th>Reason</th>
+                <th width="200">Approval</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>
+                    <div class="form-group">
+                        <div class="">
+                            <input type="text" class="form-control" name="when" id="ledgerWhen" placeholder="When" autocomplete="off" required>
+                        </div>
+                    </div>
+                </td>
+                <td>
+                    <div class="form-group">
+                        <select name="from" id="from" class="form-control">
+                            @foreach($accounts as $account)
+                            <option value="{{ $account }}">{{ $account }}</option>
+                            @endforeach @foreach($orders as $order)
+                            <option value="{{ $order }}">{{ $order }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </td>
+                <td>
+                    <div class="form-group">
+                        <select name="to" id="to" class="form-control">
+                            @foreach($accounts as $account)
+                            <option value="{{ $account }}">{{ $account }}</option>
+                            @endforeach @foreach($orders as $order)
+                            <option value="{{ $order }}">{{ $order }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </td>
+                <td>
+                    <div class="form-group">
+                        <div class="input-group">
+                            <div class="input-group-addon">
+                                <i class="la la-inr"></i>
+                            </div>
+                            <input type="number" class="form-control" name="amount" id="amount" placeholder="Amount" autocomplete="off" required>
+                        </div>
+                    </div>
+                </td>
+                <td>
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="reason" id="reason" placeholder="Reason" autocomplete="off" required>
+                    </div>
+                </td>
+                <td>
+                    <button class="btn btn-primary">
+                        <span class="ks-icon">
+                            <i class="la la-plus"></i>
+                        </span>
+                        <span class="ks-text">
+                            Add
+                        </span>
+                    </button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</form>
--- resources/views/trips/ledgers/partials/_index.blade.php
+++ resources/views/trips/ledgers/partials/_index.blade.php
@@ -1,79 +1,5 @@
 @if($showForm)
-<table class="table table-bordered">
-    <thead>
-        <tr>
-            <th>When</th>
-            @if($showOrder)
-            <th>Order</th>
-            @endif
-            <th>From</th>
-            <th>To</th>
-            <th>Amount</th>
-            <th>Reason</th>
-            <th width="200">Approval</th>
-        </tr>
-    </thead>
-    <tbody>
-        <form action="{{ url("trips/{$trip->id}/ledgers") }}" method="post"> {!! csrf_field() !!}
-            <tr>
-                <td>
-                    <div class="form-group">
-                        <div class="">
-                            <input type="text" class="form-control" name="when" id="ledgerWhen" placeholder="When" autocomplete="off" required>
-                        </div>
-                    </div>
-                </td>
-                <td>
-                    <div class="form-group">
-                        <select name="from" id="from" class="form-control">
-                            @foreach($accounts as $account)
-                            <option value="{{ $account }}">{{ $account }}</option>
-                            @endforeach @foreach($orders as $order)
-                            <option value="{{ $order->customer }}">{{ $order->customer }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                </td>
-                <td>
-                    <div class="form-group">
-                        <select name="to" id="to" class="form-control">
-                            @foreach($accounts as $account)
-                            <option value="{{ $account }}">{{ $account }}</option>
-                            @endforeach @foreach($orders as $order)
-                            <option value="{{ $order->customer }}">{{ $order->customer }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                </td>
-                <td>
-                    <div class="form-group">
-                        <div class="input-group">
-                            <div class="input-group-addon">
-                                <i class="la la-inr"></i>
-                            </div>
-                            <input type="number" class="form-control" name="amount" id="amount" placeholder="Amount" autocomplete="off" required>
-                        </div>
-                    </div>
-                </td>
-                <td>
-                    <div class="form-group">
-                        <input type="text" class="form-control" name="reason" id="reason" placeholder="Reason" autocomplete="off" required>
-                    </div>
-                </td>
-                <td>
-                    <button class="btn btn-primary">
-                        <span class="ks-icon">
-                            <i class="la la-plus"></i>
-                            </span>
-                        <span class="ks-text">
-                                Add
-                            </span>
-                    </button>
-                </td>
-            </tr>
-        </form>
-    </tbody>
-</table>
+    @include("trips.ledgers.partials._create")
 @endif
 <table class="table table-striped table-bordered dataTable" style="min-width: 600px">
     <thead>
@@ -119,7 +45,7 @@
                 <div class="ks-items-block">
                     <div class="row">
                         <div class="col">
-                            <badge class="badge badge-default">{{ $ledger->approvalStatus() }}</badge>
+                            <button class="badge badge-default">{{ $ledger->approvalStatus() }}</button>
                         </div>
                         <div class="col">
                             @if(!$ledger->approval)
